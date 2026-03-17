fa2cdd91d48ff06829e57e5e9a07d5e24e9658a7
gangasagar12 <ganga15bca2024@kcc.edu.np>
2026-03-17 14:50:00 +0545

    first commit of project

---
+++ models/Category.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace income_and_expenses_tracker.Models
+{
+    public class Category
+    {
+        public int Categoryid { get; set; }
+
+        [Column(TypeName ="nvchar(50")]
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string Category { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Type { get; set; } // Income or Expense
+    }
+}
---
+++ models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace income_and_expenses_tracker.models
+{
+    public class Transaction
+        [key]
+        public int TransctionId { get; set; }
+
+        // category id
+        public int category
+}
